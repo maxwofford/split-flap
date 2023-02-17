4b46d6d876e1931f1c11af134c42c18f173ad856
Max Wofford <max@maxwofford.com>
2023-02-17 06:23:27 -0500

    Start working on message passing

---
+++ message-passing.ino
@@ -0,0 +1,56 @@
+/* Simple test of message passing. This sketch can be loaded onto multiple
+ * devices, then daisy-chain them. If "1347" is passed via serial to the first device in the chain...
+ - the first device will flash once
+ - second device will flash three times
+ - the third device will flash four times
+ - the "7" is inconsequential and will be dropped
+ *
+ * It should also be noted this is a sequential example. This is intentional due
+ * to power constraints of the project (driving multiple steppers)
+ */
+
+int pinOut = 0;
+int pinIn = 0;
+
+void setup() {
+  while (!Serial) { }
+  Serial.begin(19200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void blinkFor(long sec) {
+  for (int i = 2; i < sec; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+  }
+}
+
+void handleMessage(char first_character) {
+  if (!first_character) return;
+
+  if (first_character >= '0' && first_character <= '9') {
+    long digit = first_character - '0';
+    blinkFor(digit);
+  }
+}
+
+void passMessage(String message) {
+  if (message.length() == 0) return;
+  // something here...
+}
+
+void loop() {
+  if (Serial.available() == 0) return;
+
+  char first_character = Serial.read();
+  String other_characters = "";
+  while (Serial.available() != 0) {
+    other_characters += Serial.read();
+  }
+
+  handleMessage(first_character);
+  passMessage(other_characters);
+}
